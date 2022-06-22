e46248f5e6d529575e6b7126aa06f17ee59e8f46
njabulodark <njabuloadvocate2001@gmail.com>
2022-06-22 18:27:55 +0200

    only nav bar added

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="with=device-width, initial-scale=1.0">
+    <title>Website Tutorial</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@1,400;1,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <section class="nav">
+        <nav>
+            <div class="logo" id="logo">
+                <h1>
+                    <a href="a.php">University Guardian</a>
+                </h1>
+            </div>
+            <div class="nave-links" id="navLinks">
+                <i class="fa fa-times" onclick="hideMenu()"></i>
+                <ul>
+                    <li><a href="">HOME</a></li>
+                    <li><a href="">Collage</a></li>
+                    <li><a href="">Pic to PDF</a></li>
+                    <li><a href="">Edit PDF</a></li>
+                    <li><a href="">CONTACT</a></li>
+                </ul>
+            </div>
+            <i class="fa fa-bars" onclick="showMenu()"></i>
+        </nav>
+    </section>
+
+    <section>
+        <p>Let's go bicth</p>
+        <p>this is a brench</p>
+    </section>
+
+<script>
+
+var navLinks = document.getElementById("navLinks");
+
+function showMenu(){
+    navLinks.style.right = "0";
+}
+
+function hideMenu(){
+    navLinks.style.right = "-300px";
+}
+</script>
+</body>
+</html>
