e6118fb5f0b77791403865add6c65f07655865d7
njabulodark <njabuloadvocate2001@gmail.com>
2022-06-23 14:51:43 +0200

    added university section

--- index.php
+++ index.php
@@ -21,6 +21,7 @@
                 <i class="fa fa-times" onclick="hideMenu()"></i>
                 <ul>
                     <li><a href="">HOME</a></li>
+                    <li><a href="">University</a></li>
                     <li><a href="">Collage</a></li>
                     <li><a href="">Pic to PDF</a></li>
                     <li><a href="">Edit PDF</a></li>
@@ -40,6 +41,52 @@
         </div>
         
     </section>
+
+    <section class="universities">
+    <div class="university">
+
+        <h1>University</h1>
+        <div class="row">
+            <div class="course-col">
+                <h3>Cape Peninsula University of Technology (CPUT)</h3>
+                <p>Website: <a href="https://www.cput.ac.za/" target="_blank">cput.ac.za</a></p>
+                <p>Online application: <a href="https://enroll.cut.ac.za/pls/prodi41/w99pkg.mi_login" target="_blank">click here</a></p>
+				<p>Application Fee: R100</p>
+				<p style="color:#00d500">Applications: OPEN</p>
+				<p>Open/Closing Date: 31 August 2022</p>
+				<p>Contact: 021 959 6767</p>
+				<p>Email: <a href="mailto:info@cput.ac.za">info@cput.ac.za</a></p>
+				<p>Location: Western Cape, Cape Town</p>
+            </div>
+            <div class="course-col">
+            <h3>Durban University of Technology (DUT)</h3>
+				<p>Application form NOT available</p>
+				<p>Website: <a href="more-info/durban-university-of-technology.html" target="_blank"> dut.ac.za</a></p>
+				<p>Online application: <a href="https://mercury.dut.ac.za/pls/prodi41/w03pkg.call_dyn_val" target="_blank">click here</a></p>
+				<p>Application Fee: R220</p>
+				<p style="color:#00d500">Applications: OPEN</p>
+				<p>Open/Closing Date: September 2022</p>
+				<p>Contact: 031 373 2000</p>
+				<p>Email: <a href="mailto:info@dut.ac.za">info@dut.ac.za</a></p>
+				<p>Location: KwaZulu Natal, Durban &amp; Pietermaritzburg</p>
+            </div>
+            <div class="course-col">
+            <h3>University of Johannesburg (UJ)</h3>
+				<p><a href="forms/UJ-2022-Application-Form.pdf" target="_blank">Click here to download 2021/22 Application form</a></p>
+				<p>Website: <a href="https://www.uj.ac.za" target="_blank">uj.ac.za</a></p>
+				<p>Online application: <a href="https://apply.online.uj.ac.za/Start-Now/Apply-Now" target="_blank">click here</a></p>
+				<p>Application Fee: R200, Online: Free</p>
+				<p style="color:#00d500">Applications: OPEN</p>
+				<p>Open/Closing Date: 30 September 2022</p>
+				<p>Contact: 011 559 3129</p>
+				<p>Email: <a href="mailto:mylife@uj.ac.za">mylife@uj.ac.za</a></p>
+				<p>Location: Gauteng, Johannesburg</p>
+            </div>
+        </div>
+        <a href="" class="hero-btn">Click here for more</a>
+        <h1 class="space"></h1>
+    </div>
+</section>
 
 <script>
 
