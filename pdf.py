06d22b503ed667e83d85c5518eb94c86a5ad6d8f
njabulodark <njabuloadvocate2001@gmail.com>
2022-09-09 21:48:42 +0200

    fix pdf path

--- pdf.py
+++ pdf.py
@@ -4,8 +4,8 @@ import os
 # opening from filename
 path = "temp/"
 for file_name in os.listdir(path):
-	file_name = os.path.splitext(f"/temp/{file_name}")[0][6:]
-	print(file_name)
+	file_name = os.path.splitext(f"temp/{file_name}")[0]
+	file_name = file_name[file_name.find("/")+1:]
 	try:
 		with open(f"pdf/{file_name}.pdf","wb") as f:
 			f.write(img2pdf.convert(f'temp/{file_name}.jpg'))
