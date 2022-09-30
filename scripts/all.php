63b670eb94fcd8a7a5a6a6584f7702946a657d7d
Nkanyiso Mzobe <sergeantmzobe@gmail.com>
2022-09-30 17:45:32 +0200

    ch

--- scripts/all.php
+++ scripts/all.php
@@ -14,6 +14,8 @@ echo $object->createTableCareer($conn);
 //create a table called stat it for changing of dates feature
 $object->createTableStat($conn);
 
+//create a table called contact for insert
+
 
 ?>
 
