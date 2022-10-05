76f1b3250093a80a899a413ceb887b307bcfa7d6
MusaIP12 <108057465+MusaIP12@users.noreply.github.com>
2022-10-05 14:30:04 +0200

    Add files via upload

---
+++ Signin.php
@@ -0,0 +1,35 @@
+<?php
+include_once "main.php";
+$object = Singleton::getInstance();
+
+$conn = $object->startDB("userregistration");
+
+
+//$object->insertIntoTableUserRegistration($conn, "nja", "nja", "nja@gmail.com");
+
+?>
+<?php
+
+$name = $_POST['acc-username'];
+$pass = $_POST['acc-password'];
+
+$sate = $object->signInUser($conn, $name, $pass);
+
+if($sate){
+    $_SESSION['username'] = $name;
+    $_SESSION['p_assword'] = $pass;
+    $_SESSION['user_email'] = $email;
+    $_SESSION['type_'] = $object->userType($conn, $name);
+    $_SESSION['logged_in'] = true;
+
+    //time the session
+    $_SESSION['start'] = time();
+    $_SESSION['expire'] = $_SESSION['start'] + (5 * 60);
+    header('location:index.php');
+}else{
+    header('location:relogin.php');
+}
+
+
+
+?>
