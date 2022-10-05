d37f4aba3797e4eb5b5d0971e9d89d128f726583
njabulodark <njabuloadvocate2001@gmail.com>
2022-10-06 01:48:57 +0200

    cre

--- Createagain.php
+++ Createagain.php
@@ -1,52 +1,175 @@
+<?php 
+include "main.php";
+$object = Singleton::getInstance();
+$conn = $object->startDB("userregistration");
+if (isset($_POST["name"]) && isset($_POST["password"]) && isset($_POST["email"])){
+$name=$_POST['username'];
+$pass=$_POST['password'];
+$email=$_POST['email'];
+
+$object->insertIntoTableContact($conn, $name, $phone, $email);
+
+if (isset($_POST['submit'])){
+  header("location: Registration.php");
+  exit;
+}
+}
+/*
+$conn = mysqli_connect('localhost', 'root', '', 'userregistration') 
+ or die('Error connecting to MySQL server.'); 
+ $query = "INSERT  INTO Signup(name, phone, email) VALUES('$name','$phone','$email')"; 
+ $result = mysqli_query($conn, $query) 
+ or die('Error querying database.'); 
+ if (isset($_POST['submit'])){
+     header("Location: Registration.php");
+     exit;
+ }
+ mysqli_close($conn);
+}
+
+*/
+?>
+
 <!DOCTYPE html>
-<html>
-<head>
-    <meta name="viewport" content="with=device-width, initial-scale=1.0">
-    <title>home</title>
-    <link rel="stylesheet" type="text/css" href="stylesheet3.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@1,400;1,700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link href="https://fonts.googleapis.com/css2?family=League+Gothic&display=swap" rel="stylesheet">
+ <html>
+ <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Signup</title>
+  <link rel="stylesheet" href="Accformstyle.css" media="all">
+  <script type="text/javascript">
+        function validateName() {
+var name = document.getElementById('username').value;
+if(name.length == 0) {
+  producePrompt('Name is required', 'name-error' , 'red')
+  return false;
+}
+if(name.length < 2){
+  producePrompt('Name is atleast two characters', 'name-error','red')
+  return false;
+}
+if (!name.match(/^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/)) {
+  producePrompt('First name, please.','name-error', 'red');
+  return false;
+}
+producePrompt('Valid','name-error','green');
+return true;
+}
+function validatePassword() {
+var pass = document.getElementById('password').value;
+if(pass.length == 0) {
+producePrompt('Password cannot be left blank', 'password-error', 'red');
+return false;
+}
+if(pass.length < 5 ) {
+producePrompt('Password must be at least 5 digits', 'password-error', 'red');
+return false;
+}
+
+producePrompt( 'Valid','password-error','green');
+return true;
+}
+function validateEmail () {
+var email = document.getElementById('email').value;
+if(email.length == 0) {
+producePrompt('Input a valid email.','email-error', 'red');
+return false;
+}
+if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+producePrompt('Email Invalid', 'email-error', 'red');
+return false;
+}
+producePrompt('Valid', 'email-error', 'green');
+return true;
+}
+function validateForm() {
+if (!validateName() && !validatePassword() && !validateEmail()) {
+jsShow('submit-error');
+producePrompt('Incorr', 'submit-error', 'red');
+setTimeout(function(){jsHide('submit-error');}, 2000);
+return false;
+}
+else if (validateName() && !validatePassword() && !validateEmail()) {
+  jsShow('submit-error');
+  producePrompt('Please fix errors to submit.', 'submit-error', 'red');
+  setTimeout(function(){jsHide('submit-error');}, 2000);
+return false;
+}
+else if(validateName() && validatePhone() && !validateEmail()){
+  jsShow('submit-error');
+  producePrompt('Please fix errors to submit.', 'submit-error', 'red');
+  setTimeout(function(){jsHide('submit-error');}, 2000);
+return false;
+ }
+ else if(validateName() && validatePassword() && validateEmail()){
+  jsShow('submit-error');
+  producePrompt('Please fix errors to submit.', 'submit-error', 'red');
+  setTimeout(function(){jsHide('submit-error');}, 2000);
+return false;
+ }
+ else if(validateName() && !validatePassword() && validateEmail() ){
+  jsShow('submit-error');
+  producePrompt('Please fix errors to submit.', 'submit-error', 'red');
+  setTimeout(function(){jsHide('submit-error');}, 2000);
+return false;
+ }
+ else if(!validateName() && validatePassword() && !validateEmail()){
+  jsShow('submit-error');
+  producePrompt('Please fix errors to submit.', 'submit-error', 'red');
+  setTimeout(function(){jsHide('submit-error');}, 2000);
+return false;
+ }
+ else if(!validateName() && validatePassword() && validateEmail()){
+  jsShow('submit-error');
+  producePrompt('Please fix errors to submit.', 'submit-error', 'red');
+  setTimeout(function(){jsHide('submit-error');}, 2000);
+return false;
+ }
+ else if(!validateName() && !validatePassword() && validateEmail()){
+  jsShow('submit-error');
+  producePrompt('Please fix errors to submit.', 'submit-error', 'red');
+  setTimeout(function(){jsHide('submit-error');}, 2000);
+return false;
+ }
+}
+function jsShow(id) {
+document.getElementById(id).style.display = 'block';
+}
+function jsHide(id) {
+document.getElementById(id).style.display = 'none';
+}
+function producePrompt(message, promptLocation, color) {
+document.getElementById(promptLocation).innerHTML = message;
+document.getElementById(promptLocation).style.color = color;
+}
+</script>
 </head>
 <body>
-<section class="nav">
-        <nav>
-            <div class="logo" id="logo">
-                <h1>
-                    <a>University Guardian</a>
-                    <h2>Your Second Parent</h2>
+  <div class="container">
+    <form action="Registration.php" method="POST" id= "formvalidation" class="form">
 
-                </h1>
-            </div>
-        </nav>
-        
-<div class="Sign-up-container">
-    <div class="Sign-upBox">
-    <h3>Create Account</h3>
-    <form action="Registration.php" method="post">
-        <div class="form-group">
-            <h6>Username already exists!!</h6>
-            <br><label>Username</label><br>
-            <input type="text" name="username" class="form-requirement" required>
-        </div>
-        <div class="form-group">
-            <br><label>Email</label><br>
-            <input type="email" name="user_email" class="form-requirement" placeholder="type email address" required>
-        </div>
-        
-        <div class="form-group">
-            <br><label>Password</label><br>
-            <input type="password" name="password" class="form-requirement" required>
-        </div>
-        <button type="submit" class="muss">Create </button>
-        <a href="login.php">Back</a>
-        </form>
-
+      <div class="form-group">
+        <h1>Username already exists</h1>
+        <h1>Create Account</h1>
+        <label for="username">Username</label>
+        <input type="text" class="form-control" id="username" name="username"   placeholder="Enter username.." onkeyup='validateName()'required>
+        <span class='error-message' id='name-error'></span>
     </div>
-    
-</div>       
+
+    <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" class="form-control" id="password" name="password" placeholder="Enter password" onkeyup='validatePassword()'required>
+        <span class='error-message' id='password-error'></span>
+    </div>
+    <div class="form-group">
+        <label for="email">Email address</label>
+        <input type="email" class="form-control" id="email" name="email" placeholder="Enter Email" onkeyup='validateEmail()'required>
+        <span class='error-message' id='email-error'></span>
+    </div>   
+    <button type = "submit" class="btn" name= "submit" onclick='return validateForm()' >Create</button>
+    <span class='error-message' id='submit-error'></span>
+</form>
+  </div>
+
 </body>
 </html>
